0b1d9ed176b29fd9e8fcc69f9be3e77d01c60c0d
Wes Hargrove <wesleyhargrove@gmail.com>
2022-12-05 19:24:47 -0700

    Add Day 1 Solution

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,27 @@
+use std::{fs::File, io::{BufReader, BufRead}};
+use log::{info};
+
 fn main() {
-    println!("Hello, world!");
+    pretty_env_logger::init();
+    let input = File::open("./day1-input").unwrap();
+    let lines = BufReader::new(input).lines()
+        .map(|l| l.unwrap());
+    let mut max = 0;
+    let mut buf = 0;
+    for line in lines {
+        // add i32 value to buf if parse
+        // or check if buf is greater than max and swap
+        if let Some(calorie) = line.parse::<i32>().ok() {
+            buf = buf + calorie;
+            info!("+{calorie}, buf: {buf}");
+        } else {
+            info!("buf: {buf}, max: {max}");
+            if buf > max {
+                info!("!!! buf: {buf} is the new max");
+                max = buf;
+            }
+            buf = 0;
+        }
+    }
+    println!("Max carried calories: {max}");
 }
