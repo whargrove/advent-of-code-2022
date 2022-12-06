977e65b6797ad9db224644fa80405fef02db203c
Wes Hargrove <wesleyhargrove@gmail.com>
2022-12-05 19:49:50 -0700

    Second gold star!

--- src/main.rs
+++ src/main.rs
@@ -1,27 +1,25 @@
-use std::{fs::File, io::{BufReader, BufRead}};
-use log::{info};
+use std::{fs::File, io::{BufReader, BufRead}, collections::BinaryHeap};
 
 fn main() {
     pretty_env_logger::init();
     let input = File::open("./day1-input").unwrap();
     let lines = BufReader::new(input).lines()
         .map(|l| l.unwrap());
-    let mut max = 0;
+    let mut heap: BinaryHeap<i32> = BinaryHeap::new();
     let mut buf = 0;
     for line in lines {
         // add i32 value to buf if parse
         // or check if buf is greater than max and swap
         if let Some(calorie) = line.parse::<i32>().ok() {
             buf = buf + calorie;
-            info!("+{calorie}, buf: {buf}");
         } else {
-            info!("buf: {buf}, max: {max}");
-            if buf > max {
-                info!("!!! buf: {buf} is the new max");
-                max = buf;
-            }
+            heap.push(buf);
             buf = 0;
         }
     }
-    println!("Max carried calories: {max}");
+    let mut sum = 0;
+    for _ in 1..=3 {
+        sum = sum + heap.pop().unwrap();
+    }
+    println!("Max carried calories: {sum}");
 }
