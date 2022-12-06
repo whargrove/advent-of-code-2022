a37369092481bc5276aec11c99d6b472f4d24b74
Wes Hargrove <wes@hargrove.me>
2022-12-06 08:37:56 -0700

    Add Day 3 solution; part one

--- src/main.rs
+++ src/main.rs
@@ -1,101 +1,46 @@
-use std::{fs::File, io::{BufReader, BufRead}};
+use std::{fs::File, io::{BufReader, BufRead}, collections::{HashSet, HashMap}};
 
-enum Move {
-    Rock,
-    Paper,
-    Scissors
-}
-
-impl From<String> for Move {
-    fn from(move_str: String) -> Self {
-        match move_str.as_str() {
-            "A" => Move::Rock,
-            "B" => Move::Paper,
-            "C" => Move::Scissors,
-            _ => panic!()
-        }
-    }
-}
-
-enum Outcome {
-    Lose,
-    Draw,
-    Win
-}
-
-impl From<String> for Outcome {
-    fn from(outcome_str: String) -> Self {
-        match outcome_str.as_str() {
-            "X" => Outcome::Lose,
-            "Y" => Outcome::Draw,
-            "Z" => Outcome::Win,
-            _ => panic!()
-        }
-    }
-}
+use log::{info, debug};
 
 fn main() {
+
+    let rubric_str = String::from("abcdefghijklmnopqrstuvwxyz");
+    let mut item_rubric: HashMap<char, i32> = HashMap::new();
+    for (index, char) in rubric_str.char_indices() {
+        // lowercase
+        let lowercase_v = index as i32 + 1;
+        item_rubric.insert(char, lowercase_v);
+        // uppercase
+        item_rubric.insert(char.to_ascii_uppercase(), lowercase_v + 26);
+    }
+
     pretty_env_logger::init();
-    let input = File::open("./day2-input").unwrap();
-    let sum:i32 = BufReader::new(input).lines()
-        .map(|l| l.unwrap())
-        .map(|l| {
-            let mut parts = l.split(" ");
-            let theirs: Move = parts.next().unwrap().to_string().into();
-            let outcome: Outcome = parts.next().unwrap().to_string().into();
-            let mine = match outcome {
-                Outcome::Lose => match theirs {
-                    Move::Rock => Move::Scissors,
-                    Move::Paper => Move::Rock,
-                    Move::Scissors => Move::Paper,
-                },
-                Outcome::Draw => match theirs {
-                    Move::Rock => Move::Rock,
-                    Move::Paper => Move::Paper,
-                    Move::Scissors => Move::Scissors,
-                },
-                Outcome::Win => match theirs {
-                    Move::Rock => Move::Paper,
-                    Move::Paper => Move::Scissors,
-                    Move::Scissors => Move::Rock,
-                },
-            };
-            let round_score = match mine {
-                Move::Rock => {
-                    match theirs {
-                        Move::Scissors => {
-                            1 + 6
-                        },
-                        Move::Rock => {
-                            1 + 3
-                        },
-                        _ => 1,
-                    }
-                },
-                Move::Paper => {
-                    match theirs {
-                        Move::Rock => {
-                            2 + 6
-                        },
-                        Move::Paper => {
-                            2 + 3
-                        }
-                        _ => 2,
-                    }
-                },
-                Move::Scissors => {
-                    match theirs {
-                        Move::Paper => {
-                            3 + 6
-                        },
-                        Move::Scissors => {
-                            3 + 3
-                        }
-                        _ => 3,
-                    }
-                },
-            };
-            round_score
+    let input = File::open("./day3-input").unwrap();
+    let lines = BufReader::new(input).lines()
+        .map(|l|l.unwrap());
+    let mut shared_items: Vec<char> = Vec::new();
+    for rucksack in lines {
+        info!("rucksack contains: {rucksack}");
+        let mut c1_items: HashSet<char> = HashSet::new();
+        let mut c2_items: HashSet<char> = HashSet::new();
+        let midpoint = rucksack.len() / 2; // this is OK because only ASCII
+        for (index, char) in rucksack.char_indices() {
+            if index < midpoint {
+                c1_items.insert(char);
+            } else {
+                c2_items.insert(char);
+            }
+        }
+        info!("c1_items: {:?}", c1_items);
+        info!("c2_items: {:?}", c2_items);
+        for item in c1_items.intersection(&c2_items) {
+            shared_items.push(*item);
+        }
+    }
+    let sum:i32 = shared_items.iter()
+        .map(|item| {
+            debug!(">>{item}");
+            item_rubric.get(item).unwrap()
         }).sum();
-        println!("Score: {sum}")
+    info!("sum of shared item priorities: {sum}")
 }
